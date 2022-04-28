9987c3c53be66ae3b6c2836d8d5f47f239d6be48
Vaibhav Singhal <vasinghal@paloaltonetworks.com>
2022-04-28 09:42:51 -0700

    First Commit to this repo. Code is taken from https://github.com/jamesqquick/youtube-for-developers

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                'brand-50': '#FDF6F6',
+                'brand-100': '#FCEEEE',
+                'brand-200': '#F7D4D4',
+                'brand-300': '#F2BABB',
+                'brand-400': '#E88687',
+                brand: '#DE5254',
+                'brand-600': '#C84A4C',
+                'brand-700': '#853132',
+                'brand-800': '#642526',
+                'brand-900': '#431919',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
